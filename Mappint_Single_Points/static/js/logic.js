e17bf3709693a57f24c59c0c45fc32e24bdb771e
Jake Wellman <wellma41@msu.edu>
2021-12-18 04:49:37 -0500

    multiple things

--- Mappint_Single_Points/static/js/logic.js
+++ Mappint_Single_Points/static/js/logic.js
@@ -7,11 +7,24 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 //  Add a marker to the map for Los Angeles, California.
 //let marker = L.marker([34.0522, -118.2437]).addTo(map);
-L.circle([34.0522, -118.2437], {
-    color: "black",
-    fillColor:'#ffffa1',
-    radius: 300
- }).addTo(map);
+// L.circleMarker([34.0522, -118.2437], {
+//     color: "black",
+//     fillColor:'#ffffa1',
+//     radius: 300
+//  }).addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.marker(city.location, {
+        radius: city.population/10000
+    }) 
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
